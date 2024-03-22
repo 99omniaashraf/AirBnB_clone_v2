f29dea511971140a9d13f52a3bc88830c08771c6
99omniaashraf <omniaashraf249@gmail.com>
2024-03-22 22:57:50 +0200

    Hello Flask!

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-""" module doc """
+""" Start a web application """
+
+
 from flask import Flask
+
 
 app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
-def hello():
-    """ def doc """
+def Hello():
     return "Hello HBNB!"
 
 
