14999e47d27492f040f00aa6d100b937ce0a2aa7
99omniaashraf <omniaashraf249@gmail.com>
2024-03-23 01:18:44 +0200

    List of states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-""" starts a Flask web application """
-from flask import Flask
-from flask import render_template
+""" Starts a Flask web application """
+
+from flask import Flask, render_template
 from models import storage
 from models.state import State
 
@@ -10,30 +10,32 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
-    """ def doc """
+    """ Display "Hello HBNB!" """
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """ Display "HBNB" """
     return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
-    # Replace underscores with spaces in the text variable
+    """ Display "C <text>" where underscores are replaced with spaces """
     return 'C {}'.format(text.replace("_", " "))
 
 
 @app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python(text):
-    """ def doc """
+    """ Display "Python <text>" where underscores are replaced with spaces """
     return 'Python {}'.format(text.replace("_", " "))
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def show_number(n):
+    """ Display "<n> is a number" if n is an integer """
     if isinstance(n, int):
         return '{} is a number'.format(n)
     else:
@@ -42,26 +44,25 @@ def show_number(n):
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def show_odd_or_even(n):
+    """ Display a template with information about whether the number is odd or even """
     if isinstance(n, int):
-        if n % 2 == 0:
-            p = 'even'
-        else:
-            p = 'odd'
-        return render_template('6-number_odd_or_even.html', number=n, parity=p)
+        parity = 'even' if n % 2 == 0 else 'odd'
+        return render_template('6-number_odd_or_even.html', number=n, parity=parity)
     else:
         return "404 Not Found"
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """ def doc """
+    """ Display a list of states sorted by name """
     states = storage.all(State)
-    return render_template('7-states_list.html', states=states)
+    sorted_states = sorted(states.values(), key=lambda state: state.name)
+    return render_template('7-states_list.html', states=sorted_states)
 
 
 @app.teardown_appcontext
 def close(error):
-    """ def doc """
+    """ Close the storage engine """
     storage.close()
 
 
